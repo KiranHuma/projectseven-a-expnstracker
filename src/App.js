c076cef0ca9fc2ef880e389956cded8e2a655223
aimalhuma61 <66478525+aimalhuma61@users.noreply.github.com>
2020-08-16 08:07:17 +0500

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
-import { initNotification } from './services/firebaseService'
+// import { initNotification } from './services/firebaseService'
 // import BGImage from './boder.jpg';
 import { GlobalProvider } from './context/GlobalState';
 import './App.css';
